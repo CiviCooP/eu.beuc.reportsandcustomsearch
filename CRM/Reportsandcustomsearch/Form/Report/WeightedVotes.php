557bfbb4941613704f451b36e77b28313ed759e1
Alain Benbassat <alain@businessandcode.eu>
2024-11-07 15:57:17 +0100

    next

--- CRM/Reportsandcustomsearch/Form/Report/WeightedVotes.php
+++ CRM/Reportsandcustomsearch/Form/Report/WeightedVotes.php
@@ -18,8 +18,14 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
     $cols = [];
 
     $colTitles = [
-      'id' => 'Contact ID',
-      'display_name' => 'Naam',
+      'display_name' => 'Member',
+      'country' => 'Country',
+      'membership_type' => 'Membership Type',
+      'membership_fee' => 'Fee Previous Year',
+      'membership_fee_percentage' => '% of Total',
+      'membership_fee_status' => 'Payment Status',
+      'voting_rights' => 'Voting rights?',
+      'number_of_votes' => 'Number of Votes',
     ];
 
     $i = 1;
@@ -37,7 +43,26 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
   }
 
   private function getReportFilters() {
-    return [];
+    $currentYear = intval(date('Y'));
+    $toYear = $currentYear - 5;
+    
+    $years = [];
+    for ($i = $currentYear + 1; $i >= $toYear; $i--) {
+      $years[$i] = $i;
+    }
+    
+    $filters = [
+      'voting_year' => [
+        'title' => 'Voting Year',
+        'dbAlias' => '1',
+        'type' => CRM_Utils_Type::T_INT,
+        'operatorType' => CRM_Report_Form::OP_SELECT,
+        'options' => $years,
+        'default' => $currentYear,
+      ],
+    ];
+
+    return $filters;
   }
 
   function preProcess() {
@@ -59,18 +84,122 @@ class CRM_Reportsandcustomsearch_Form_Report_WeightedVotes extends CRM_Report_Fo
   }
 
   public function alterDisplay(&$rows) {
+    $votingYear = $this->getVotingYear();
+
     // build the report from scratch
     $rows = [];
 
-    $referenceYear = date('Y'); // TODO: get from parameter in filter
-    $members = new CRM_Reportsandcustomsearch_Members($referenceYear);
-    foreach ($members->get() as $member) {
+    $members = new CRM_Reportsandcustomsearch_Members($votingYear);
+    $allMembers = $members->get();
+    $totalFees = 0;
+
+    foreach ($allMembers as $member) {
       $row = [];
 
-      $row['civicrm_dummy_entity_id'] = $members['contact_id'];
-      $row['civicrm_dummy_entity_display_name'] = $members['contact_id.display_name'];
+      $row['civicrm_dummy_entity_display_name'] = $this->getNameWithLinkToContact($member);
+      $row['civicrm_dummy_entity_country'] = $member['address.country_id:label'];
+      $row['civicrm_dummy_entity_membership_type'] = $member['membership_type_id:label'];
+
+      [$fee, $status] = $members->getMembershipFee($member['contact_id']);
+      $row['civicrm_dummy_entity_membership_fee'] = $fee;
+      $row['civicrm_dummy_entity_membership_fee_percentage'] = '';
+      $row['civicrm_dummy_entity_membership_fee_status'] = $status;
+      $row['civicrm_dummy_entity_voting_rights'] = '';
+      $row['civicrm_dummy_entity_number_of_votes'] =  0;
+
+      $totalFees += $row['civicrm_dummy_entity_membership_fee'];
 
       $rows[] = $row;
     }
+
+    $totalNumberOfVotes = 0;
+    $totalVotingRights = 0;
+
+    for ($i = 0; $i < count($rows); $i++) {
+      if ($totalFees > 0) {
+        $percentageFee = round($rows[$i]['civicrm_dummy_entity_membership_fee'] / $totalFees * 100, 2);
+      }
+      else {
+        $percentageFee = 0;
+      }
+
+      $rows[$i]['civicrm_dummy_entity_membership_fee_percentage'] =  $percentageFee;
+
+      if ($rows[$i]['civicrm_dummy_entity_membership_type'] !== 'Full Member') {
+        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'No, not a full member';
+        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  0;
+      }
+      elseif ($rows[$i]['civicrm_dummy_entity_membership_fee'] == 0) {
+        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'No, no fee';
+        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  0;
+      }
+      elseif ($rows[$i]['civicrm_dummy_entity_membership_fee_status'] !== 'Paid') {
+        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'No, not paid';
+        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  0;
+      }
+      else {
+        $rows[$i]['civicrm_dummy_entity_voting_rights'] =  'Yes';
+        $totalVotingRights++;
+
+        if ($percentageFee <= 1) {
+          $numVotes = 1;
+        }
+        elseif ($percentageFee <= 5) {
+          $numVotes = 3;
+        }
+        else {
+          $numVotes = 5;
+        }
+        $rows[$i]['civicrm_dummy_entity_number_of_votes'] =  $numVotes;
+      }
+
+      $totalNumberOfVotes += $rows[$i]['civicrm_dummy_entity_number_of_votes'];
+    }
+
+    // totals
+    $row = [
+      'civicrm_dummy_entity_display_name' => '',
+      'civicrm_dummy_entity_country' => '',
+      'civicrm_dummy_entity_membership_type' => '',
+      'civicrm_dummy_entity_membership_fee' => $totalFees,
+      'civicrm_dummy_entity_membership_fee_percentage' => '',
+      'civicrm_dummy_entity_membership_fee_status' => '',
+      'civicrm_dummy_entity_voting_rights' => $totalVotingRights,
+      'civicrm_dummy_entity_number_of_votes' => $totalNumberOfVotes,
+    ];
+    $this->makeBold($row);
+    $rows[] = $row;
+
+    $row = [
+      'civicrm_dummy_entity_display_name' => '',
+      'civicrm_dummy_entity_country' => '',
+      'civicrm_dummy_entity_membership_type' => '',
+      'civicrm_dummy_entity_membership_fee' => '',
+      'civicrm_dummy_entity_membership_fee_percentage' => 'MINIMUM',
+      'civicrm_dummy_entity_membership_fee_status' => '2/3',
+      'civicrm_dummy_entity_voting_rights' => ceil($totalVotingRights * 2 / 3),
+      'civicrm_dummy_entity_number_of_votes' => ceil($totalNumberOfVotes * 2 / 3),
+    ];
+    $this->makeBold($row);
+    $rows[] = $row;
+
   }
+
+  private function makeBold(&$row) {
+    foreach ($row as $k => $v) {
+      $row[$k] = "<b>$v</b>";
+    }
+  }
+
+  private function getNameWithLinkToContact($member) {
+    return '<a target=_blank href="' . CRM_Utils_System::url('civicrm/contact/view', 'reset=1&cid=' . $member['contact_id']) . '">' . $member['contact_id.display_name'] . '</a>';
+  }
+
+  private function getVotingYear() {
+    $values =  $this->exportValues();
+    
+    return $values['voting_year_value'];
+  }
+
+
 }
--- CRM/Reportsandcustomsearch/Members.php
+++ CRM/Reportsandcustomsearch/Members.php
@@ -1,19 +1,46 @@
 <?php
 
 class CRM_Reportsandcustomsearch_Members {
-  public $referenceYear;
+  public $votingYear;
+  public $votingYearMinusOne;
 
-  public function __construct(int $referenceYear) {
-    $this->referenceYear = $referenceYear;
+  public function __construct(int $votingYear) {
+    $this->votingYear = $votingYear;
+    $this->votingYearMinusOne = $votingYear - 1;
   }
 
   public function get() {
     return \Civi\Api4\Membership::get(FALSE)
-      ->addSelect('id', 'contact_id', 'contact_id.display_name', 'membership_type_id:label')
+      ->addSelect('id', 'contact_id', 'contact_id.display_name', 'membership_type_id:label', 'address.country_id:label')
+      ->addJoin('Address AS address', 'LEFT', ['contact_id', '=', 'address.contact_id'], ['address.is_primary', '=', 1])
       ->addWhere('is_primary_member', '=', TRUE)
-      ->addWhere('start_date', '<=', $this->referenceYear . '-12-31')
-      ->addWhere('end_date', '>=', $this->referenceYear . '-12-31')
+      ->addWhere('start_date', '<=', $this->votingYear . '-12-31')
+      ->addWhere('end_date', '>=', $this->votingYear . '-12-31')
       ->addOrderBy('contact_id.display_name', 'ASC')
       ->execute();
+  }
+
+  public function getMembershipFee($contactId) {
+    $sql = "
+      select 
+        ifnull(sum(total_amount), 0) fee,
+        contribution_status_id
+      from 
+        civicrm_contribution 
+      where 
+        contact_id = $contactId 
+      and 
+        year(receive_date) = {$this->votingYearMinusOne} 
+      and 
+        financial_type_id = 2
+    ";
+
+    $dao = CRM_Core_DAO::executeQuery($sql);
+    if ($dao->fetch()) {
+      return [$dao->fee, $dao->contribution_status_id == 1 ? 'Paid' : 'Not paid'];
+    }
+    else {
+      return [0, ''];
+    }
   }
 }
